e39f622165096968cf468a9454c500e105d1833b
Utsav Patel <utsav.swaroop@gmail.com>
2014-04-15 20:23:30 +0100

    test project

---
+++ TestProject/TestProject1/TestProject1ViewController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace TestProject1
+{
+	public partial class TestProject1ViewController : UIViewController
+	{
+		public TestProject1ViewController () : base ("TestProject1ViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			System.Console.WriteLine ("View did load");
+		}
+
+		partial void DisplayButtonPress (NSObject sender)
+		{
+			DisplayLabel.Text = "Hello World";
+//			UIAlertView alert = new UIAlertView ("Title", "Display AlertView", null , "OK", "Cancel");
+//			alert.Show();
+
+//			UIAlertView alert = new UIAlertView ("Alert Title", "Choose from two buttons", null, "OK", new string[] {"Cancel"});
+//			alert.Clicked += (s, b) => {
+//				DisplayLabel.Text = "Button " + b.ButtonIndex.ToString () + " clicked";
+//				Console.WriteLine ("Button " + b.ButtonIndex.ToString () + " clicked");
+//			};
+//			alert.Show();
+
+			UIAlertView alert = new UIAlertView () {
+				Title = "custom buttons alert", 
+				Message = "this alert has custom buttons"
+			};
+			alert.AddButton("OK");
+			alert.AddButton("custom button 1");
+			alert.AddButton("Cancel");
+
+			alert.Clicked += delegate(object a, UIButtonEventArgs b) {
+				DisplayLabel.Text = "Button " + b.ButtonIndex.ToString () + " clicked";
+				Console.WriteLine ("Button " + b.ButtonIndex.ToString () + " clicked"); };
+			alert.Show ();
+		}
+	}
+}
+
---
+++ TestProject/TestProject1/TestProject1ViewController.designer.cs
@@ -0,0 +1,37 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace TestProject1
+{
+	[Register ("TestProject1ViewController")]
+	partial class TestProject1ViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton DisplayButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel DisplayLabel { get; set; }
+
+		[Action ("DisplayButtonPress:")]
+		partial void DisplayButtonPress (MonoTouch.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (DisplayLabel != null) {
+				DisplayLabel.Dispose ();
+				DisplayLabel = null;
+			}
+
+			if (DisplayButton != null) {
+				DisplayButton.Dispose ();
+				DisplayButton = null;
+			}
+		}
+	}
+}
